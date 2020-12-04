4b0614cad9967284137c51cd4c6e52b54a5cbc5e
DalduK <przemyslaw7wozny@gmail.com>
2020-12-04 16:33:03 +0100

    Password and Register Views rozwijanie

--- InzSwiftUIApp/InzSwiftUIApp/PassView.swift
+++ InzSwiftUIApp/InzSwiftUIApp/PassView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct PassView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State var userName: String = ""
+    @State var password: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let colors = Gradient(colors: [.purple,.blue])
+        let gradient = LinearGradient(gradient: colors, startPoint: .bottomLeading, endPoint: .topTrailing)
+        ZStack{
+            VStack {
+                Text("Przypomnij hasło")
+                    .bold()
+                    .font(.title)
+                
+                Text("Podaj mail powiązany z kontem")
+                
+                Image("Password")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
+                HStack {
+                    Image(systemName: "envelope").foregroundColor(.gray)
+                    TextField("Email",text:$userName)
+                }
+                .padding(.init(top: 10, leading: 20, bottom: 10, trailing: 20))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10).stroke(gradient,lineWidth: 5)
+                )
+                .padding(.init(top: 10, leading: 15, bottom: 5, trailing: 15))
+                
+                ZStack{
+                    Button(action: {
+                        withAnimation {
+                            
+                    }
+                    }){
+                        Text("Zaloguj się")
+                            .padding()
+                            .background(gradient)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+                .padding(.top,5)
+            }
+        }
+        .padding(.horizontal)
     }
+    
 }
 
 struct PassView_Previews: PreviewProvider {
--- InzSwiftUIApp/InzSwiftUIApp/RegisterView.swift
+++ InzSwiftUIApp/InzSwiftUIApp/RegisterView.swift
@@ -8,9 +8,88 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var userName: String = ""
+    @State var email: String = ""
+    @State var password: String = ""
+    @EnvironmentObject var userAuth: UserAuth
+    @State private var rules = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    let colors = Gradient(colors: [.purple,.blue])
+    let gradient = LinearGradient(gradient: colors, startPoint: .bottomLeading, endPoint: .topTrailing)
+        ZStack{
+            VStack {
+                Text("Zarejestruj się!")
+                    .bold()
+                    .font(.title)
+                
+                Image("Register")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
+                HStack {
+                    Image(systemName: "person").foregroundColor(.gray)
+                    TextField("Nick",text:$userName)
+                }
+                .padding(.init(top: 10, leading: 20, bottom: 10, trailing: 20))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10).stroke(gradient,lineWidth: 5)
+                )
+                .padding(.init(top: 10, leading: 15, bottom: 5, trailing: 15))
+                
+                HStack {
+                    Image(systemName: "envelope").foregroundColor(.gray)
+                    SecureField("Email",text:$userName)
+                }
+                .padding(.init(top: 10, leading: 20, bottom: 10, trailing: 20))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10).stroke(gradient,lineWidth: 5)
+                )
+                .padding(.init(top: 10, leading: 15, bottom: 5, trailing: 15))
+                
+                HStack {
+                    Image(systemName: "lock").foregroundColor(.gray)
+                    TextField("Password",text:$userName)
+                }
+                .padding(.init(top: 10, leading: 20, bottom: 10, trailing: 20))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10).stroke(gradient,lineWidth: 5)
+                )
+                .padding(.init(top: 10, leading: 15, bottom: 5, trailing: 15))
+                
+                HStack {
+                    Image(systemName: "lock").foregroundColor(.gray)
+                    SecureField("Password",text:$userName)
+                }
+                .padding(.init(top: 10, leading: 20, bottom: 10, trailing: 20))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10).stroke(gradient,lineWidth: 5)
+                )
+                .padding(.init(top: 10, leading: 15, bottom: 5, trailing: 15))
+                
+                Toggle("Potwierdź regularmin", isOn: $rules)
+                    .padding(.init(top: 10, leading: 15, bottom: 5, trailing: 15))
+                
+                
+                ZStack{
+                    Button(action: {
+                        withAnimation {
+                            userAuth.login()
+                    }
+                    }){
+                        Text("Zarejestruj się")
+                            .padding()
+                            .background(gradient)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+                .padding(.top,5)
+            }
+        }
+        .padding(.horizontal)
     }
+    
 }
 
 struct RegisterView_Previews: PreviewProvider {
