5dcfc58449cefbdb75eb14590f429aaefd89743d
DalduK <przemyslaw7wozny@gmail.com>
2020-12-04 09:36:10 +0100

    Base of A project

---
+++ InzSwiftUIApp/InzSwiftUIApp/InzSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InzSwiftUIAppApp.swift
+//  InzSwiftUIApp
+//
+//  Created by Przemysław Woźny on 04/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct InzSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
