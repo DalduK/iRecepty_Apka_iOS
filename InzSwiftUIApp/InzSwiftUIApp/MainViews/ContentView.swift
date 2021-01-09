8528089d23cf85a7d57a3a50d31a89b36b178cb4
DalduK <przemyslaw7wozny@gmail.com>
2021-01-09 20:06:43 +0100

    Project grouped

---
+++ InzSwiftUIApp/InzSwiftUIApp/MainViews/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  InzSwiftUIApp
+//
+//  Created by Przemysław Woźny on 04/12/2020.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @EnvironmentObject var userAuth: UserAuth
+    @State var showMenu = false
+    var body: some View {
+        GeometryReader { geo in
+            if !userAuth.isLoggeddin {
+                LoginView()
+            } else {
+                HomeView()
+                    .padding(.top)
+                    .frame(width: geo.size.width, height: geo.size.height)
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environmentObject(UserAuth())
+    }
+}
